0865b9c59d15c6cf082ea6bbaa8eb7c8a668e7e3
Craig Puxty <craig_puxty@talktalk.net>
2021-08-18 21:36:56 +0100

    getStaticPaths fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 const Post = ({ post }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div className="container">
       <h2>
@@ -12,23 +20,23 @@ const Post = ({ post }) => {
 export default Post;
 
 export const getStaticPaths = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const data = await res.json();
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //   const data = await res.json();
 
-  const paths = data.map((post) => {
-    return { params: { postId: `${post.id}` } };
-  });
+  //   //   const paths = data.map((post) => {
+  //   //     return { params: { postId: `${post.id}` } };
+  //   //   });
 
   return {
-    // paths: [
-    //   { params: { postId: "1" } },
-    //   { params: { postId: "2" } },
-    //   { params: { postId: "3" } },
-    //   { params: { postId: "4" } },
-    //   { params: { postId: "5" } },
-    // ],
-    paths,
-    fallback: false,
+    paths: [
+      { params: { postId: "1" } },
+      { params: { postId: "2" } },
+      { params: { postId: "3" } },
+      { params: { postId: "4" } },
+      { params: { postId: "5" } },
+    ],
+    // paths,
+    fallback: true,
   };
 };
 
@@ -38,5 +46,11 @@ export const getStaticProps = async (context) => {
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
   );
   const data = await res.json();
+
+  if (!data.id) {
+    return { notFound: true };
+  }
+
+  console.log(`Generating page for /posts/${params.postId}`);
   return { props: { post: data } };
 };
